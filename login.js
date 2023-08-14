aac3106a1e224ffd5f656648f6e12d2e7f18b6c1
sudip <paudelsudip134@gmail.com>
2023-08-14 14:09:37 +0545

    login js upd

---
+++ login.js
@@ -0,0 +1,18 @@
+
+  //showing password
+  showLoginPassword=document.getElementById("toggle-password");
+  showLoginPassword.addEventListener("click",()=>{
+    loginPassword=document.getElementById("login-password");
+      if (loginPassword.type == "password") {
+        loginPassword.type = "text";
+      } else {
+        loginPassword.type = "password";
+      }
+  });
+  //login with third party warn--->
+  const logins=document.getElementsByClassName("login-with");
+  Array.from(logins).forEach(login => {
+    login.addEventListener("click",()=>{
+      alert("This service will be added soon!! Try another way for now------>");
+    });
+  });
