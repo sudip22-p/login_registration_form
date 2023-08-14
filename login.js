930517ef0a38ad8af7001bdfba62481999a36224
sudip <paudelsudip134@gmail.com>
2023-08-14 18:22:32 +0545

    eye-slash updated

--- login.js
+++ login.js
@@ -2,6 +2,8 @@
   //showing password
   showLoginPassword=document.getElementById("toggle-password");
   showLoginPassword.addEventListener("click",()=>{
+    showLoginPassword.classList.toggle("fa-eye-slash");
+    showLoginPassword.classList.toggle("fa-eye");
     loginPassword=document.getElementById("login-password");
       if (loginPassword.type == "password") {
         loginPassword.type = "text";
